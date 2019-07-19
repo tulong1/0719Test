483c77b50f363a6ee445ada9a13ca234df05123c
hjh001 <1418642797@qq.com>
2019-07-19 13:04:28 +0800

    init

---
+++ src/main/java/com/newer/github0719/Github0719Application.java
@@ -0,0 +1,13 @@
+package com.newer.github0719;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Github0719Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Github0719Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/newer/github0719/Github0719ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.newer.github0719;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Github0719ApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
